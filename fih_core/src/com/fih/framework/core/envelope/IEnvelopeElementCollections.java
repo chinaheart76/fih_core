64c94c7e7be33d694d6cae707b4467483f7a0a39
shaofuan <chinaheart76@gmail.com>
2016-03-24 17:02:31 +0800

    增加信封处理接口

--- fih_core/src/com/fih/framework/core/envelope/IEnvelopeElementCollections.java
+++ fih_core/src/com/fih/framework/core/envelope/IEnvelopeElementCollections.java
@@ -3,5 +3,7 @@ package com.fih.framework.core.envelope;
 import java.util.Collection;
 
 public interface IEnvelopeElementCollections {
+	
 		Collection<IEnvelopeElementDescription> getCollections();
+		
 }
---
+++ fih_core/src/com/fih/framework/core/envelope/IEnvelopeHandler.java
@@ -0,0 +1,7 @@
+package com.fih.framework.core.envelope;
+
+public interface IEnvelopeHandler {
+	
+	IEnvelope handler(IEnvelope envelope);
+
+}
